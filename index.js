06a157e14a045cb60efb889b6264c5fb0c5a8c37
Pranav Shekhar <pranavshekhar00@gmail.com>
2025-03-24 16:42:44 +0530

    changing index file

--- index.js
+++ index.js
@@ -29,8 +29,11 @@ app.use(express.json());
 
 
 // connecting with database
-const connectDB = require("./src/db");
-connectDB();
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DB_STRING
+).then(()=>{
+    console.warn("db connection done again")
+})
 
 // Pass the `io` instance to routes
 app.use((req, res, next) => {
